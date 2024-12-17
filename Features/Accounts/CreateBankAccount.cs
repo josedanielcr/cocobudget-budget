e43e2a704fc4b4815dd1ccc0c00a4647c72401cc
josedanielcr <jdcanales1905@gmail.com>
2024-12-16 23:06:38 -0600

    Feat: creation of bank accounts

--- Features/Accounts/CreateBankAccount.cs
+++ Features/Accounts/CreateBankAccount.cs
@@ -1,8 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using Carter;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using web_api.Contracts.Account.Requests;
 using web_api.Contracts.Account.Responses;
 using web_api.Database;
+using web_api.Entities;
+using web_api.Features.Category;
 using web_api.Shared;
 
 namespace web_api.Features.Accounts;
@@ -34,9 +39,88 @@ public static class CreateBankAccount
     internal sealed class Handler(ApplicationDbContext dbContext, IValidator<Command> validator)
         : IRequestHandler<Command, Result<BankAccountResponse>>
     {
-        public Task<Result<BankAccountResponse>> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<BankAccountResponse>> Handle(Command request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            
+            var validationResult = await ValidateRequestAsync(request, cancellationToken);
+            if (!validationResult.IsSuccess)
+                return Result.Failure<BankAccountResponse>(validationResult.Error);
+            
+            var newBankAccount = new BankAccount
+            {
+                Name = request.Name,
+                BankName = request.BankName,
+                CurrentBalance = request.CurrentBalance,
+                Currency = request.Currency,
+                AccountNumber = request.AccountNumber,
+                Notes = request.Notes
+            };
+            
+            await dbContext.BankAccounts.AddAsync(newBankAccount, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            
+            return new BankAccountResponse(
+                newBankAccount.Id,
+                newBankAccount.IsActive,
+                newBankAccount.CreatedOn,
+                newBankAccount.ModifiedOn,
+                newBankAccount.Name,
+                newBankAccount.BankName,
+                newBankAccount.CurrentBalance,
+                newBankAccount.Currency,
+                newBankAccount.AccountNumber,
+                newBankAccount.Notes
+            );
         }
+        
+        private async Task<Result> ValidateRequestAsync(Command request, CancellationToken cancellationToken)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return Result.Failure(new Error("CreateBankAccount.Validation", validationResult.ToString()));
+            }
+
+            var bankAccountExists = await dbContext.BankAccounts
+                .AnyAsync(x => x.AccountNumber == request.AccountNumber, cancellationToken);
+            if (bankAccountExists)
+            {
+                return Result.Failure(new Error("CreateBankAccount.AccountNumber", "Account number already exists"));
+            }
+
+            if (request.CurrentBalance < 0)
+            {
+                return Result.Failure(new Error("CreateBankAccount.CurrentBalance", "Current balance cannot be negative"));
+            }
+
+            return Result.Success();
+        }
+    }
+}
+
+public class CreateBankAccountEndpoint : ICarterModule
+{
+    private const string RouteTag = "Accounts";
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/accounts/bank", async (CreateBankAccountRequest request, ISender sender) =>
+        {
+            var command = new CreateBankAccount.Command
+            {
+                Name = request.Name,
+                BankName = request.BankName,
+                CurrentBalance = request.CurrentBalance,
+                Currency = request.Currency,
+                AccountNumber = request.AccountNumber,
+                Notes = request.Notes
+            };
+
+            var result = await sender.Send(command);
+
+            return result.IsFailure
+                ? Results.BadRequest(result)
+                : Results.Ok(result);
+        }).WithTags(RouteTag);
     }
 }
