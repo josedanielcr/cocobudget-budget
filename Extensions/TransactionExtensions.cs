f41fd57b4e0329adfa35f8a80ea29fa72a5e432e
josedanielcr <jdcanales1905@gmail.com>
2024-12-30 17:50:39 -0600

    Feat: deletion of transactions and rollback of effects in categories and accounts

--- Extensions/TransactionExtensions.cs
+++ Extensions/TransactionExtensions.cs
@@ -28,6 +28,32 @@ public static class TransactionExtensions
         
         CreateTransactionEffectToCategory(dbContext, transaction, category);
     }
+    
+    public static void HandleExpenseTransactionDelete(ApplicationDbContext dbContext, BankAccount account,
+        Category category, TransactionCategoryEffect effect)
+    {
+        category.AmountSpent -= effect.Amount;
+        category.AmountRemaining = category.TargetAmount - category.AmountSpent;
+        
+        // Custom categories have a target that should increase when a transaction is deleted / fixed categories don't
+        if (category.GeneralCategory.CategoryType == CategoryType.Custom)
+        {
+            category.GeneralCategory.TargetAmount += effect.Amount;
+        }
+        
+        // If the transaction was in a different currency, the effect will have a conversion rate to return the bank account to the original amount
+        if (effect.ConversionRate != null)
+        {
+            account.CurrentBalance += effect.Amount / (effect.ConversionRate ?? 1);
+        }
+        else
+        {
+            account.CurrentBalance += effect.Amount;
+        }
+        
+        dbContext.Categories.Update(category);
+        dbContext.BankAccounts.Update(account);
+    }
 
     /**
      * once its affected it logs into a Effect table between the transaction and the category so it can be used
